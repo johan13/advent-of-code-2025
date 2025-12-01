3b14b8bcfc19eceef423dc3bece9cdd3a3b09a41
Johan Levin <johan13@gmail.com>
2025-12-01 20:26:52 +0100

    Day 1

--- day01-bash/day01.sh
+++ day01-bash/day01.sh
@@ -1,47 +1,40 @@
 #!/usr/bin/env bash
 
 part1() {
-    # Read the two columns into col1 and col2.
-    local -a col1 col2
-    while read -r a b; do
-        col1+=("$a")
-        col2+=("$b")
+    local pos=50 zeros=0
+    while read -r line; do
+        local direction="${line:0:1}"
+        local count="${line:1}"
+        [[ "$direction" == L ]] && ((count = -count))
+        ((pos = ((pos + count) % 100 + 100) % 100))
+        ((pos == 0 )) && ((zeros++))
     done
-
-    # Sort the two arrays.
-    mapfile -t col1 < <(printf '%s\n' "${col1[@]}" | sort -n)
-    mapfile -t col2 < <(printf '%s\n' "${col2[@]}" | sort -n)
-
-    # Sum up the absolute values of the differences.
-    local sum=0 diff
-    for i in "${!col1[@]}"; do
-        ((diff = col1[i] - col2[i]))
-        ((sum += ${diff#-}))
-    done
-    echo "$sum"
+    echo "$zeros"
 }
 
 part2() {
-    # Read the two columns into col1 and col2.
-    local -a col1 col2
-    while read -r a b; do
-        col1+=("$a")
-        col2+=("$b")
+    local pos=50 zeros=0
+    while read -r line; do
+        local direction="${line:0:1}"
+        local count="${line:1}"
+        if [[ "$direction" == R ]]; then
+            ((pos += count))
+            while ((pos >= 100)); do
+                ((pos -= 100))
+                ((zeros++))
+            done
+        else
+            ((pos == 0)) && ((zeros--)) # Special case for starting at zero.
+            ((pos -= count))
+            while ((pos < 0)); do
+                ((pos += 100))
+                ((zeros++))
+            done
+            ((pos == 0)) && ((zeros++)) # Special case for stopping at zero.
+        fi
     done
-
-    # Count the occurrences in col2.
-    local -A counts
-    for n in "${col2[@]}"; do
-        ((counts[$n]++))
-    done
-
-    # Multiply each col1 value with the corresponding count from col2 and sum up the products.
-    local sum=0
-    for n in "${col1[@]}"; do
-        ((sum += n * ${counts[$n]:-0}))
-    done
-    echo "$sum"
+    echo "$zeros"
 }
 
-echo "Day 01 part 1: $(part1 < input.txt)"
-echo "Day 01 part 2: $(part2 < input.txt)"
+echo "Day 01 part 1: $(part1 < input.txt)" # 1031
+echo "Day 01 part 2: $(part2 < input.txt)" # 5831
