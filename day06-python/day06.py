0209c88ddb4fd535949d752cbe3d63687fdb0654
Johan Levin <johan13@gmail.com>
2025-12-06 19:17:22 +0100

    Day 6 part 2

--- day06-python/day06.py
+++ day06-python/day06.py
@@ -1,10 +1,27 @@
-import math
-import sys
+from math import prod
+from sys import stdin
 
-lines = [line.split() for line in sys.stdin]
-answer = 0
-for *operands, operator in zip(*lines):
-    nums = [int(x) for x in operands]
-    answer += sum(nums) if operator == "+" else math.prod(nums)
+def apply_op(operator, str_operands):
+    operands = map(int, str_operands)
+    return sum(operands) if operator == "+" else prod(operands)
 
-print(f"Day 06 part 1: {answer}")
+def part1(lines):
+    return sum(
+        apply_op(op, nums)
+        for *nums, op in zip(*[line.split() for line in lines])
+    )
+
+def part2(lines):
+    # Ignore empty columns. Instead, assume that the operator is in the first column of each problem.
+    columns = [c for c in ("".join(row) for row in zip(*lines)) if c.strip()]
+    answer, operands = 0, []
+    for col in reversed(columns):
+        operands.append(col[:-1])
+        if col[-1] in "+*":
+            answer += apply_op(col[-1], operands)
+            operands = []
+    return answer
+
+lines = stdin.read().splitlines()
+print(f"Day 06 part 1: {part1(lines)}") # 5877594983578
+print(f"Day 06 part 2: {part2(lines)}") # 11159825706149
