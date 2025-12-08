afdb2207ade6b135ab7a90d8c2fee5353bb1ae29
Johan Levin <johan13@gmail.com>
2025-12-08 12:07:52 +0100

    Day 7 part 1

--- day07-fsharp/day07.fs
+++ day07-fsharp/day07.fs
@@ -1,2 +1,22 @@
-﻿printfn "Day 07 part 1: TODO"
+﻿open System
+
+let rec countSplits beams = function
+    | [] -> 0
+    | (firstLine: string)::lines ->
+        let (splitting, notSplitting) = beams |> List.partition (fun b -> firstLine[b] = '^')
+        let newBeams =
+            (splitting |> List.collect (fun b -> [b-1; b+1])) @ notSplitting
+            |> List.distinct
+        List.length splitting + countSplits newBeams lines
+
+let part1 = function
+    | [] -> 0
+    | (firstLine: string)::lines -> countSplits [firstLine.IndexOf('S')] lines
+
+let lines =
+    Seq.initInfinite (fun _ -> Console.ReadLine())
+    |> Seq.takeWhile (fun line -> line <> null)
+    |> Seq.toList
+
+printfn $"Day 07 part 1: {part1 lines}" // 1537
 printfn "Day 07 part 2: TODO"
